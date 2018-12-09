6b9cf5ef557f7bb8c1b64309caa7aed0f3d2b0ee
Huong Hk <huonghk.uit@gmail.com>
2018-12-09 22:19:44 +0700

    Normalize Iso Date

--- src/API/article/normalize.js
+++ src/API/article/normalize.js
@@ -1,5 +1,15 @@
 import placeholderSmall from './placeholder-252.png';
 
+// The pub_date returned by NYTimes API is not correct with ISO_8601 format
+// https://en.wikipedia.org/wiki/ISO_8601
+// E.g: 2018-11-29T08:26:15+0000
+// This will lead to error to convert to Date object in some browsers
+const normalizeIsoDate = (str) => {
+  const part1 = str.slice(0, -2);
+  const part2 = str.slice(-2);
+  return `${part1}:${part2}`;
+};
+
 const getMultimediaUrl = ({ multimedia, type, placeholder }) => {
   const obj = multimedia.find(item => item.subtype === type);
   return obj ? `https://www.nytimes.com/${obj.url}` : placeholder;
@@ -16,7 +26,7 @@ const normalizeArticles = (response) => {
       small: getMultimediaUrl({ multimedia: item.multimedia, type: 'smallSquare252', placeholder: placeholderSmall }),
       large: getMultimediaUrl({ multimedia: item.multimedia, type: 'popup' }),
     },
-    pubDate: item.pub_date,
+    pubDate: normalizeIsoDate(item.pub_date),
     source: item.source,
   }));
 };
